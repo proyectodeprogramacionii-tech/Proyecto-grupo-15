461279309927b37a56813e3de317485c0005b271
proyectodeprogramacionii-tech <proyectodeprogramacionii@gmail.com>
2026-06-11 11:59:27 -0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,5 +1,18 @@
 import codecs;
-
+def string_a_lista(linea):
+    """
+    segun el dato que necesitamos;devuelve la cantidad de comas necesarias para llegar a el
+    """
+    buscador=""
+    comas=[]
+    for char in linea:
+            if char!=",":
+                buscador+=char
+            elif buscador!="":
+                 comas.append(buscador)
+                 buscador=""
+    comas.append(buscador)
+    return comas
 # Lectura del dataset
 archivo = open("Motor_Vehicle_Collisions_sample_12K.csv")
 linea = archivo.readline()

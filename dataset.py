6626d7cc57fedaaffb1c639024ee143443f49b4e
valentinol <61915336+valentinol@users.noreply.github.com>
2026-06-11 23:44:02 -0300

    marco para una estructura de datos diccionario de listas, investigar salida

--- dataset.py
+++ dataset.py
@@ -4,20 +4,35 @@ def string_a_lista(linea):
     segun el dato que necesitamos;devuelve la cantidad de comas necesarias para llegar a el
     """
     buscador=""
-    comas=[]
+    lista=[]
     for char in linea:
             if char!=",":
                 buscador+=char
-            elif buscador!="":
-                 comas.append(buscador)
+            elif buscador==",":
+                 lista.append(buscador)
                  buscador=""
-    comas.append(buscador)
-    return comas
-# Lectura del dataset
-archivo = open("Motor_Vehicle_Collisions_sample_12K.csv")
-linea = archivo.readline()
-while linea != '':
-    print(linea)
-    linea = archivo.readline()
+    lista.append(buscador)
+    return lista
 
+def dic(lista):
+    titulos=lista[0]
+    demas_data=lista[1:]
+    long=len(demas_data[0])
+    dicc={}
+    var=0
+    for titulo in titulos:
+        dicc[titulo]=[]
+        for linea in demas_data:
+            dicc[titulo].append(linea(var))
+        var+=1
+    return dicc
+    
+def main():
+    archivo = open("Motor_Vehicle_Collisions_sample_12K.csv")
+    linea = archivo.readline()
+    lista=[]
+    while linea != '':
+        linea = archivo.readline()
+        lista=string_a_lista(lista)
+        
 archivo.close()

4f9e27de78cd6cacaf5d714e4e7161b95ea3c7a8
valentinol <61915336+valentinol@users.noreply.github.com>
2026-06-18 13:58:36 +0000

    finalizacion de la pregunta estatica

--- proyecto.py
+++ proyecto.py
@@ -77,12 +77,31 @@ def mayores_cinco(diccionario):
     max_variable4=["",0]
     max_variable5=["",0]
     for clave in diccionario:
-        if max_variable1[1]<diccionario[clave]:
-            max_variable1=[clave,diccionario[clave]]
-            max_variable2=[max_variable1[0],max_variable1[1]]
-            max_variable3=[max_variable2[0],max_variable2[1]]
+        valor=diccionario[clave]
+        if max_variable1[1]<valor:
+            max_variable5=[max_variable4[0],max_variable4[1]]
             max_variable4=[max_variable3[0],max_variable3[1]]
-            max_variable5=[max_variable4[0],max_variable5[1]]
+            max_variable3=[max_variable2[0],max_variable2[1]]
+            max_variable2=[max_variable1[0],max_variable1[1]]
+            max_variable1=[clave,valor]
+        
+        elif max_variable2[1]<valor:
+            max_variable5=[max_variable4[0],max_variable4[1]]
+            max_variable4=[max_variable3[0],max_variable3[1]]
+            max_variable3=[max_variable2[0],max_variable2[1]]
+            max_variable2=[clave,valor]
+        
+        elif max_variable3[1]<valor:
+            max_variable5=[max_variable4[0],max_variable4[1]]
+            max_variable4=[max_variable3[0],max_variable3[1]]
+            max_variable3=[clave,valor]
+       
+        elif max_variable4[1]<valor:
+            max_variable5=[max_variable4[0],max_variable4[1]]
+            max_variable4=[clave,valor]
+        
+        elif max_variable5[1]<valor:
+            max_variable5=[clave,valor]
     los_5[max_variable1[0]]=max_variable1[1]
     los_5[max_variable2[0]]=max_variable2[1]
     los_5[max_variable3[0]]=max_variable3[1]
@@ -90,27 +109,47 @@ def mayores_cinco(diccionario):
     los_5[max_variable5[0]]=max_variable5[1]
     return los_5
 
+print(mayores_cinco({
+    "a":1000,
+    "b":5,
+    "c":800,
+    "d":20,
+    "e":700,
+    "f":10,
+    "g":600
+}))
 def total_de_5(diccionario):
     var=0
-    for item in diccionario.items():
+    for item in diccionario.values():
         var+=item
-    return item
+    return var
         
 
 
 
 def grafico_torta(diccionario):
     le_5=mayores_cinco(diccionario)
+    nom=[]
+    for x in le_5:
+        nom.append(x)
     total=total_de_5(le_5)
-    nom=le_5.keys()
     labels = nom[0], nom[1], nom[2], nom[3],nom[4]
     sizes = [le_5[nom[0]]/total, le_5[nom[1]]/total, le_5[nom[2]]/total, le_5[nom[3]]/total,le_5[nom[4]]/total]
-    fig, ax = plt.subplots()
+    fig , ax = plt.subplots()
     ax.pie(sizes, labels=labels)
+    return fig
+
+
+
 
 #def lista_de_accidentes():
 def main():
     estructura_datos=dic(diccionario())
-    print(conteo_autos(lista_de_autos(estructura_datos)))
+    dicc=(conteo_autos(lista_de_autos(estructura_datos)))
+    grafico_torta(dicc)
+    #print(mayores_cinco(dicc))
+    fig = grafico_torta(dicc)
+    st.pyplot(fig)
+    print(total_de_5(mayores_cinco(dicc)))
 
 main()
